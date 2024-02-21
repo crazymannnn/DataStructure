0bc4757d25fc47d877aafb5c9999de792564c889
DESKTOP-9ABEO1B\ADMIN <duynguyen120504@gmail.com>
2024-02-21 22:19:17 +0700

    This homework i convert long string (>10 characters) into shorter string.

--- Way Too Long Words/Program.cs
+++ Way Too Long Words/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace Way_Too_Long_Words
+﻿using System.Security.Cryptography;
+
+namespace Way_Too_Long_Words
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Reader reader = new Reader();
+            int n = reader.NextInt();
+            string[] arr = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = reader.Next();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int count = arr[i].Length;
+                if (arr[i].Length <= 10)
+                {
+                    Console.WriteLine(arr[i].ToLower());
+                }
+                if (arr[i].Length > 10)
+                {
+                    string first = arr[i][0].ToString();
+                    string last = arr[i][count - 1].ToString();
+                    Console.WriteLine(first.ToLower() + (count - 2) + last.ToLower());
+                }
+            }
+        }
+    }
+    class Reader
+    {
+        private int index = 0;
+        private string[] tokens;
+        public string Next()
+        {
+            while (tokens == null || tokens.Length <= index)
+            {
+                tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                index = 0;
+            }
+            return tokens[index++];
+        }
+        public int NextInt()
+        {
+            return int.Parse(Next());
+        }
+
+        public double NextDouble()
+        {
+            return double.Parse(Next());
         }
     }
 }
