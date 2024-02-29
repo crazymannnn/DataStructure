64519488d3df5c260ec6d5a2a948ba3ee38e4ff3
DESKTOP-9ABEO1B\ADMIN <duynguyen120504@gmail.com>
2024-02-29 21:25:06 +0700

    First 2 excercise on SPOJ

---
+++ LINQ Product ID NAME PRICE/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace LINQ_Product_ID_NAME_PRICE
+{
+    internal class Program //duy
+    {
+        static void Main(string[] args)
+        {
+            Reader reader = new Reader();
+            int start = reader.NextInt();
+            int end = reader.NextInt();
+            var youwant = reader.Next();
+            var product1 = new Product()
+            {
+                Id = "94831",
+                Price = 15000,
+                Name = "meat chicken",
+                Kind = "meat",
+                Quantity = 10,
+            };
+            var product2 = new Product()
+            {
+                Id = "29316",
+                Price = 20000,
+                Name = "water sugar",
+                Kind = "water",
+                Quantity = 20,
+            };
+            var product3 = new Product() { 
+                Id = "23014",
+                Price = 30000,
+                Name = "meat duck",
+                Kind = "meat",
+                Quantity = 30,
+            };
+            var product4 = new Product() {
+                Id = "23132",
+                Price = 40000,
+                Name = "water drink",
+                Kind = "water",
+                Quantity = 40,
+            };
+
+            var giohang = new List<Product>();
+            giohang.Add(product1);
+            giohang.Add(product2);
+            giohang.Add(product3);
+            giohang.Add(product4);
+
+            var finalresult = FindTheRemaindingQuantity(giohang);
+            foreach (var stuff in finalresult) {
+                Console.WriteLine($"{stuff.Kind}: {stuff.Quantity}");
+            }
+            
+
+            //for (int i = 0; i < productfound.Count(); i++) {
+            //    sum = sum + productfound[i].Quantity;
+            //}
+
+        }
+
+
+        public static List<Report> FindTheRemaindingQuantity(List<Product> giohang) {
+            var result = giohang
+                .GroupBy(i => i.Kind)
+                .Select(i => new Report()
+                {
+                    Kind = i.Key,
+                    Quantity = i.Sum(x => x.Quantity),
+                })
+                .ToList();
+            return result;
+        }
+
+        public class Report {
+            public string Kind { get; set; }
+            public int Quantity { get; set; }
+        }
+
+        public class Product { 
+            public string Id { get; set; }
+            public int Price { get; set; }
+            public string Name { get; set; }
+            public string Kind { get; set; }
+            public int Quantity { get; set; }
+        }
+
+        class Reader
+        {
+            private int index = 0;
+            private string[] tokens;
+            public string Next()
+            {
+                while (tokens == null || tokens.Length <= index)
+                {
+                    tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    index = 0;
+                }
+                return tokens[index++];
+            }
+            public int NextInt()
+            {
+                return int.Parse(Next());
+            }
+
+            public double NextDouble()
+            {
+                return double.Parse(Next());
+            }
+        }
+    }
+}
---
+++ spoj.EIDUPBOD/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+namespace spoj.EIDUPBOD
+{
+    internal class Program //duy
+    {
+        static void Main(string[] args)
+        {
+            Reader reader = new Reader();
+            var student = reader.NextInt();
+            var dayofbirth = new List<Dob>();
+            //nhap input
+            for (int i = 0; i < student; i++) {
+                var dob = new Dob()
+                {
+                    Date = reader.NextInt(),
+                    Month = reader.NextInt(),
+                    Year = reader.NextInt(),
+                };
+                dayofbirth.Add(dob);
+                
+            }
+            var check = dayofbirth
+                .GroupBy(x => new { x.Date, x.Month, x.Year })
+                .Select(x => x)
+                .OrderBy(x => x.Key.Year)
+                .ThenBy(x => x.Key.Month)
+                .ThenBy(x => x.Key.Date)
+                .ToList();
+
+            foreach (var people in check) {
+                //if (people.Key.Month < 10 && people.Key.Date < 10)
+                //{
+                //    Console.WriteLine($"0{people.Key.Date}/0{people.Key.Month}/{people.Key.Year} {people.Count()}");
+                //}
+                //else if (people.Key.Month >= 10 && people.Key.Date < 10)
+                //{
+                //    Console.WriteLine($"0{people.Key.Date}/{people.Key.Month}/{people.Key.Year} {people.Count()}");
+                //}
+                //else if (people.Key.Month < 10 && people.Key.Date >= 10)
+                //{
+                //    Console.WriteLine($"{people.Key.Date}/0{people.Key.Month}/{people.Key.Year} {people.Count()}");
+                //}
+                //else {
+                //    Console.WriteLine($"{people.Key.Date}/{people.Key.Month}/{people.Key.Year} {people.Count()}");
+                //}
+                var date = people.Key.Date.ToString();
+                if (people.Key.Date < 10) {
+                    date = $"0{date}";
+                }
+                var month = people.Key.Month.ToString();
+                if (people.Key.Month < 10) {
+                    month = $"0{month}";
+                }
+                Console.WriteLine($"{date}/{month}/{people.Key.Year} {people.Count()}");                                                   
+            }
+        }
+
+
+        public class Dob {
+            public int Date { get; set; }
+            public int Month { get; set; }
+            public int Year { get; set; }
+        }
+
+
+        class Reader
+        {
+            private int index = 0;
+            private string[] tokens;
+            public string Next()
+            {
+                while (tokens == null || tokens.Length <= index)
+                {
+                    tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    index = 0;
+                }
+                return tokens[index++];
+            }
+            public int NextInt()
+            {
+                return int.Parse(Next());
+            }
+
+            public double NextDouble()
+            {
+                return double.Parse(Next());
+            }
+        }
+    }
+}
