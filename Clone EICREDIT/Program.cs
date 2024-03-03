c701fa25d67c0cd29f9e5a65843d49fbac0041db
DESKTOP-9ABEO1B\ADMIN <duynguyen120504@gmail.com>
2024-03-03 19:42:59 +0700

    bai tap

---
+++ Clone EICREDIT/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Clone_EICREDIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader rd = new Reader();
+            var numOfstudents = rd.NextInt();
+            var liststudent = new List<People>();
+            for (int i = 0; i < numOfstudents; i++) {
+                var pp = new People
+                {
+                    Name = rd.Next(),
+                    Subject = rd.NextInt(),
+                };
+                for (int j = 0; j < pp.Subject; j++) {
+                    pp.Score.Add(rd.NextInt());
+                }
+                liststudent.Add(pp);
+            }
+            foreach (var human in liststudent) {
+                var finalresult = checkCredit(human);
+                Console.WriteLine($"{human.Name} {finalresult}");
+            }
+
+        }
+
+        public static int checkCredit(People pp) {
+            var credit = pp.Score.Count(i => i >= 50);
+            var result = credit * 4;
+            return result;
+        }
+
+        public class People {
+            public string Name { get; set; }
+            public int Subject { get; set; }
+            public List<int> Score {get; set;} = new List<int>();
+        }
+
+        class Reader
+        {
+            private int index = 0;
+            private string[] tokens;
+            public string Next()
+            {
+                while (tokens == null || tokens.Length <= index)
+                {
+                    tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    index = 0;
+                }
+                return tokens[index++];
+            }
+            public int NextInt()
+            {
+                return int.Parse(Next());
+            }
+
+            public double NextDouble()
+            {
+                return double.Parse(Next());
+            }
+        }
+    }
+}
---
+++ CloneEIUUPBOD/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace CloneEIUUPBOD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader rd = new Reader();
+            var students = rd.NextInt();
+            var listdob = new List<Dob>();
+            for (int i = 0; i < students; i++) {
+                var dob = new Dob()
+                {
+                    Date = rd.NextInt(),
+                    Month = rd.NextInt(),
+                    Year = rd.NextInt(),
+                };
+                listdob.Add(dob);
+            }
+            var check = listdob
+                .GroupBy(x => new { x.Date, x.Month, x.Year })
+                .Select(x => x)
+                .OrderBy(x => x.Key.Year)
+                .ThenBy(x => x.Key.Month)
+                .ThenBy(x => x.Key.Date)
+                .ToList();
+
+            foreach (var day in check) {
+                var date = day.Key.Date.ToString();
+                var month = day.Key.Month.ToString();
+                if (day.Key.Date < 10) {
+                    date = 0 + date;
+                }
+                if (day.Key.Month < 10)
+                {
+                    month = 0 + month;
+                }
+                Console.WriteLine($"{date}/{month}/{day.Key.Year} {day.Count()}");
+            }
+
+        }
+
+        public class Dob {
+            public int Date { get; set; }
+            public int Month { get; set; }
+            public int Year { get; set; }
+        }
+
+        class Reader
+        {
+            private int index = 0;
+            private string[] tokens;
+            public string Next()
+            {
+                while (tokens == null || tokens.Length <= index)
+                {
+                    tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    index = 0;
+                }
+                return tokens[index++];
+            }
+            public int NextInt()
+            {
+                return int.Parse(Next());
+            }
+
+            public double NextDouble()
+            {
+                return double.Parse(Next());
+            }
+        }
+    }
+}
